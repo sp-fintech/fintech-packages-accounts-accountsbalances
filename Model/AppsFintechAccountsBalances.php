ea8d3c559a1d3507d3aac9548b17d10aa59b815a
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-02-19 00:17:30 +1100

    Initial Commit

---
+++ Model/AppsFintechAccountsBalances.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Apps\Fintech\Packages\Accounts\Balances\Model;
+
+use System\Base\BaseModel;
+
+class AppsFintechAccountsBalances extends BaseModel
+{
+    public $id;
+
+    public $first_name;
+
+    public $last_name;
+}
